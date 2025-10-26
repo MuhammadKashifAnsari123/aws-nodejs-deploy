da09a17eb186874aed10c3c422e68a0c8046e0ad
MuhammadKashifAnsari123 <muhammadkashifa82@gmail.com>
2025-10-27 00:38:24 +0500

    Add user details endpoint to API

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get("/api/get", (req,res) => {
     res.send({message:"hello back to nodejs"})
 })
 
+app.get("/api/get_user_details",(req, res) => {
+    res.send({
+            user:{
+                name:"Kashif Ansari",
+                age:21,
+                contact:"03092662658",
+            }
+        })
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
