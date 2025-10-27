55fec7c9ce97076bc804d0913fab8f964c34b0d1
MuhammadKashifAnsari123 <muhammadkashifa82@gmail.com>
2025-10-27 15:38:59 +0500

    Add .env to .gitignore and configure dotenv in server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const PORT = 8000;
 
@@ -12,11 +14,9 @@ app.get("/api/get_user_details",(req, res) => {
                 name:"Kashif Ansari",
                 age:21,
                 contact:"03092662658",
-            }
+            },env:process.env.NAME
         })
 })
-
-
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
